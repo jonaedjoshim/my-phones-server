2a0a13994fdede00c0a041533be2a8d72c70b6ef
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-24 16:05:58 +0600

    creater my phones server and loaded single data using get api

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const phones = require("./phones.json");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    "my phone info is coming soon, it's under construction! sorry for the inconvenience.",
+  );
+});
+
+app.get("/phones", (req, res) => {
+  res.send(phones);
+});
+
+app.get("/phones/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("Requested phone ID:", id);
+  const phone = phones.find((phone) => phone.id === id || {});
+  res.send(phone);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
