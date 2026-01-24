c92ccdc9a0f27c13a97bab909857c63eaf7abeae
mdjonaedjoshim <mdjonaedjoshim@gmail.com>
2026-01-24 22:39:11 +0600

    ...

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const phones = require("./phones.json");
 const app = express();
 const port = 3000;
+
+app.use(cors()); 
 
 app.get("/", (req, res) => {
   res.send(
